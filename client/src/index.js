9c63d4119bbc4969a57a78f4bd5c74744ffe492e
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-05-09 11:27:31 -0600

    update cart menu

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { theme } from "./theme";
+import cartReducer from "./state";
+
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
